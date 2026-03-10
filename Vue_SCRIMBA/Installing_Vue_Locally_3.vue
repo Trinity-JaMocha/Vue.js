51ff817c7c2a1d045e452271ef0dfc61e99d28ac
Trinity-JaMocha <trinityblush@gmail.com>
2026-03-10 16:26:16 +0000

    Installing Vue Locally with Terminal

---
+++ Vue_SCRIMBA/Installing_Vue_Locally_3.vue
@@ -0,0 +1,15 @@
+<!--Type in terminal-->>
+npm create vue@latest
+<!--Then click y-->
+y
+<!--Name Project-->
+first_vue_project
+<!--Option to use Typescript (this time... no)-->
+no
+<!--Option to Select Features-->
+--- click enter button to skip
+<!--Option for Experimental Features-->
+--- click enter button to skip
+<!--Skip all example code and start with a blank vue project (to scaffold a minimal project structure)-->
+Yes
+<!--Follow instructions given by terminal-->
